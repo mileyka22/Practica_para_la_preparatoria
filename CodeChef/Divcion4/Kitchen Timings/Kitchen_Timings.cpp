f62d7c655acf3e90247f368b6400342a687bf508
mileyka22 <120768538+mileyka22@users.noreply.github.com>
2022-12-23 12:16:33 -0400

    Problema Kitchen Timings

--- CodeChef/Divcion4/Kitchen Timings/Kitchen_Timings.cpp
+++ CodeChef/Divcion4/Kitchen Timings/Kitchen_Timings.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 int main()
 {
+    int x, y, t, r;
+    cin >> t;
+    int arr1[t];
+    for (int i = 0; i < t; i++)
+    {
+
+        cin >> x >> y;
+        r = y - x;
+        arr1[i] = r;
+    }
+    for (int i = 0; i < t; i++)
+    {
+        cout << arr1[i] << endl;
+    }
 }
